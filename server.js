dd5adb3b6f3e3ea0e4b5e03566667df58a87f9a8
gambitxp41 <blankif34@gmail.com>
2026-05-06 17:50:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ cloudinary.config({
 // ========================
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
