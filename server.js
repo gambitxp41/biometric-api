587556fda75d770f2f434d52e03c5babb65a7163
gambitxp41 <blankif34@gmail.com>
2026-05-07 19:06:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -105,14 +105,13 @@ app.post("/return-reservation", async (req, res) => {
 // ===============================
 app.get("/stats/users", async (req, res) => {
     try {
-        const result = await db.query("SELECT COUNT(*) AS total FROM users");
-        res.json({ total: result[0].total });
+        const [rows] = await db.query("SELECT COUNT(*) AS total FROM users");
+        res.json({ total: rows[0].total });
     } catch (err) {
         console.error(err);
         res.json({ total: 0 });
     }
 });
-
 // ===============================
 // TOTAL INVENTORY ITEMS
 // ===============================
